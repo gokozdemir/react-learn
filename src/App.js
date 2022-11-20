88b198ea9693b0b30fa8cd5889976d3b54d9f172
Gökhan Özdemir <gokozdemir20@gmail.com>
2022-11-20 18:11:53 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,67 @@
+import { useState } from "react";
 const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
   imageSize: 90,
-}
+};
 
-let isLoggedIn = true
+let isLoggedIn = true;
 
 const products = [
-  { title: 'Cabbage', id: 1 },
-  { title: 'Garlic', id: 2 },
-  { title: 'Apple', id: 3 },
-]
+  { title: "Cabbage", id: 1 },
+  { title: "Garlic", id: 2 },
+  { title: "Apple", id: 3 },
+];
 
-function Profile(){
+function Profile() {
   return (
     <>
       <h1>{user.name}</h1>
-      <img 
-        className='avatar'
+      <img
+        className="avatar"
         src={user.imageUrl}
-        alt={'Photo of ' + user.name}
+        alt={"Photo of " + user.name}
         style={{
           width: user.imageSize,
           height: user.imageSize,
-          borderRadius: '50%'
+          borderRadius: "50%",
         }}
       />
     </>
-  )
+  );
 }
-
 
 function Greeting({ name }) {
   return <h1>Welcome to my app, {name}</h1>;
 }
 
 function MyButton() {
-
+  const [count, setCount] = useState(0);
   // Responding to Events
-  function handleClick(){
-    console.log('You clicked me!')
+  function handleClick() {
+    setCount(count + 1);
   }
-  return <button className="button" onClick={handleClick}>Click Me</button>;
+  return (
+    <button className="button" onClick={handleClick}>
+      Click {count} times
+    </button>
+  );
 }
 
 export default function App() {
-  const listItems = products.map(product => 
-    <li key={product.id} style={{color: product.isFruit ? 'magenta' : 'darkgreen'}}>
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{ color: product.isFruit ? "magenta" : "darkgreen" }}
+    >
       {product.title}
     </li>
-  )
+  ));
 
   return (
     <div>
       <Greeting name="World" />
+      <MyButton />
       <MyButton />
 
       {/*Conditional Rendering */}
@@ -61,12 +69,7 @@ export default function App() {
       {!isLoggedIn && <Profile />}
 
       {/* Rendering Lists */}
-      <ul>
-        {listItems}
-      </ul>
+      <ul>{listItems}</ul>
     </div>
   );
 }
-
-
-
